003f1bf69c2cdb5362370a89072391fc87b411b6
TD <santilopezbadra@gmail.com>
2026-07-22 19:00:11 -0300

    Escena mago
    escena del mago lista

---
+++ prueba/Escenas/Unidades/Mago/mago.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var health = 100
+var  damage= 20
+var attack_range = 100
+var attack_speed = 1 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("El mago estyá listo") # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
