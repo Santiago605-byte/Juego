1ccc389b4b69f2e9aba0fe984e99d23c4784b8b5
TD <santilopezbadra@gmail.com>
2026-07-23 10:13:03 -0300

    Mago detecta enemigos
    El mago ya detecta salida y entrada de enemigos

--- prueba/Escenas/Unidades/Mago/mago.gd
+++ prueba/Escenas/Unidades/Mago/mago.gd
@@ -1,14 +1,27 @@
 extends Node2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 var health = 100
 var  damage= 20
 var attack_range = 100
 var attack_speed = 1 
+var enemies = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("El mago estyá listo") # Replace with function body.
+	animated_sprite.play("Idle")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("algo entró")
+	enemies.append(body)
+	print(enemies.size())
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	enemies.erase(body)
+	print(enemies.size())
